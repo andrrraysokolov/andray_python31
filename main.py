5bb3d486c65f50e8336ffa9a316e322272b3ff6e
MKA <MKA@ULTOP.local>
2023-05-12 21:19:10 +0400

    ИЗМЕНИЛ ФАЙЛ main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+type_int = 1 # целые числа (int)
+type_float = 1.23 # числа с плавающей точкой
+type_str = "Строки пишутся в кавычках"
+type_bool_one = True # булевы значения истина (правда)
+type_bool_two = False # булевы значения ложь
+type_none = None
+print(type_int*2)
