35c98c8de837c90e583dc8cc0650168e22234c23
andrrraysokolov <i@andrrraysokolov.ru>
2023-05-16 21:17:07 +0400

    160523

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
 #type_int = 1 # целые числа (int)
 #type_float = 1.23 # числа с плавающей точкой
 #type_str = "Строки пишутся в кавычках"
@@ -20,12 +24,40 @@
 # z = float(input("Введите число - мы умножим его на 2: "))
 # print (z * 2)
 
-print("Заполните информацию")
-myName = input("Введите имя: ")
-myAge = int(input("Введите возраст: "))
-myCountry = input("Ваше гражданство: ")
-print("")
-print("Информация о пользователе:")
-print("Имя", myName)
-print("Ваш возраст, умноженный на 2:", myAge*2)
-print("Ваше гражданство:", myCountry)
+# print("Заполните информацию")
+# myName = input("Введите имя: ")
+# myAge = int(input("Введите возраст: "))
+# myCountry = input("Ваше гражданство: ")
+# print("")
+# print("Информация о пользователе:")
+# print("Имя", myName)
+# print("Ваш возраст, умноженный на 2:", myAge*2)
+# print("Ваше гражданство:", myCountry)
+
+# myName = "and"
+# print ("привет" + " " + myName)
+# print (f"привет {2*3}")
+# print ("привет", " ", "мир!")
+
+# z = int(input("Введите число: "))
+# print (z//10)
+# print (z%10)
+
+# z = int(input("Введите число: "))
+# a = z//100
+# b = (z//10)%10
+# c = z%10
+# d = a+b+c
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+a = str(input("Введите первую цифру: "))
+b = str(input("Введите вторую цифру: "))
+c = a+b
+c = int(c)
+print(c)
+
+
+
