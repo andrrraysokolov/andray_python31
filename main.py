b0a01600763322ac4ac7928a634afb210a5949ae
andrrraysokolov <i@andrrraysokolov.ru>
2023-05-16 21:22:09 +0400

    160523

--- main.py
+++ main.py
@@ -1,7 +1,61 @@
-type_int = 1 # целые числа (int)
-type_float = 1.23 # числа с плавающей точкой
-type_str = "Строки пишутся в кавычках"
-type_bool_one = True # булевы значения истина (правда)
-type_bool_two = False # булевы значения ложь
-type_none = None
-print(type_int*78)
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+#type_int = 1 # целые числа (int)
+#type_float = 1.23 # числа с плавающей точкой
+#type_str = "Строки пишутся в кавычках"
+#type_bool_one = True # булевы значения истина (правда)
+#type_bool_two = False # булевы значения ложь
+#type_none = None
+
+# a = 123
+# print(a * 2,type(a)) #int
+# a = str(a)
+# print (a * 2,type(a)) #str
+# a = float(a) #float
+# print(a * 2,type(a))
+
+# b = "Привет"
+# print(b,type(b))
+# b = int(b)
+# print (b,type(b))
+
+# z = float(input("Введите число - мы умножим его на 2: "))
+# print (z * 2)
+
+# print("Заполните информацию")
+# myName = input("Введите имя: ")
+# myAge = int(input("Введите возраст: "))
+# myCountry = input("Ваше гражданство: ")
+# print("")
+# print("Информация о пользователе:")
+# print("Имя", myName)
+# print("Ваш возраст, умноженный на 2:", myAge*2)
+# print("Ваше гражданство:", myCountry)
+
+# myName = "and"
+# print ("привет" + " " + myName)
+# print (f"привет {2*3}")
+# print ("привет", " ", "мир!")
+
+# z = int(input("Введите число: "))
+# print (z//10)
+# print (z%10)
+
+# z = int(input("Введите число: "))
+# a = z//100
+# b = (z//10)%10
+# c = z%10
+# d = a+b+c
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+a = str(input("Введите первую цифру: "))
+b = str(input("Введите вторую цифру: "))
+c = a+b
+c = int(c)
+print(c)
+
