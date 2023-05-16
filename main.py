b5a5956d8fc738a5e97dac7a330913d40a14c7ac
andrrraysokolov <i@andrrraysokolov.ru>
2023-05-16 18:48:51 +0400

    1234

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-type_int = 1 # целые числа (int)
-type_float = 1.23 # числа с плавающей точкой
-type_str = "Строки пишутся в кавычках"
-type_bool_one = True # булевы значения истина (правда)
-type_bool_two = False # булевы значения ложь
-type_none = None
-print(type_int*78)
+#type_int = 1 # целые числа (int)
+#type_float = 1.23 # числа с плавающей точкой
+#type_str = "Строки пишутся в кавычках"
+#type_bool_one = True # булевы значения истина (правда)
+#type_bool_two = False # булевы значения ложь
+#type_none = None
+
+a = 123
+print(a * 2,type(a)) #int
+a = str(a)
+print (a * 2,type(a)) #str
+a = float(a) #float
+print(a * 2,type(a))
+
+
